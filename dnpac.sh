6bb0f6035e9dd542a63d4982b712f691ecd7c34d
Rodrigo Munguía <38513145+rmgjp@users.noreply.github.com>
2021-07-18 01:43:32 -0500

    Add files via upload

---
+++ dnpac.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function dnpac (){
+    if((EUID != 0)); then
+        echo "Please run this script with sudo."
+    exit 0
+    else
+        case $1 in
+
+        i|install)
+            echo "Performing command: pacman -S ${@:2}"
+            pacman -S ${@:2}
+        ;;
+
+        r|remove)
+            echo "Performing command: pacman -R ${@:2}"
+            pacman -R ${@:2}
+        ;;
+
+        u|update)
+            echo "Performing command: pacman -Syu"
+            pacman -Syu
+        ;;
+        c|clean)
+            echo "Performing command: paccache"
+            paccache -r
+        ;;
+        h|help)
+            echo "Help lol"
+        ;;
+        *)
+            echo "Unknown argument, no command performed."
+        ;;
+        esac
+    exit
+    fi
+}
+dnpac $@
+
+
+
