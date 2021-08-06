6ae75eb84ff94fe7ee4218cd482937b44d5748d4
Rodrigo Munguía <38513145+rmgjp@users.noreply.github.com>
2021-08-05 20:38:36 -0500

    Update dnpac.sh
    Added "help" parameter.

--- dnpac.sh
+++ dnpac.sh
@@ -7,31 +7,36 @@ function dnpac (){
     else
         case $1 in
 
-        i|install)
-            echo "Performing command: pacman -S ${@:2}"
-            pacman -S ${@:2}
-        ;;
+            i|install)
+                echo "Performing command: pacman -S ${@:2}"
+                pacman -S ${@:2}
+            ;;
 
-        r|remove)
-            echo "Performing command: pacman -R ${@:2}"
-            pacman -R ${@:2}
-        ;;
+            r|remove)
+                echo "Performing command: pacman -R ${@:2}"
+                pacman -R ${@:2}
+            ;;
 
-        u|update)
-            echo "Performing command: pacman -Syu"
-            pacman -Syu
-        ;;
-        c|clean)
-            echo "Performing command: paccache"
-            paccache -r
-        ;;
-        h|help)
-            echo "Help lol"
-        ;;
-        *)
-            echo "Unknown argument, no command performed."
-        ;;
-        esac
+            u|update)
+                echo "Performing command: pacman -Syu"
+                pacman -Syu
+            ;;
+            c|clean)
+                echo "Performing command: paccache"
+                paccache -r
+            ;;
+            h|help)
+                echo "Operations:"
+                echo "{i install} <packages>"
+                echo "{r remove} <packages>"
+                echo "{u update}"
+                echo "{c clean} <packages>"
+                echo "{h help} <packages>"
+            ;;
+            *)
+                echo "Unknown argument, no command performed."
+            ;;
+            esac
     exit
     fi
 }
